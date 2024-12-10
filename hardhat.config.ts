ac6710c3375f745f2ae3b3da43ef49e6a531e957
shuhei tanaka <shuhei46491@gmail.com>
2024-12-10 12:25:57 +0900

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+};
+
+export default config;
