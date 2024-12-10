3bea835ff3ec4eacd749e2746726c6fdc214a1a4
shuhei tanaka <shuhei46491@gmail.com>
2024-12-10 12:58:59 +0900

    chore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"],
+        },
+      },
+    },
+  },
+  paths: { cache: "cache/hardhat" },
 };
 
 export default config;
